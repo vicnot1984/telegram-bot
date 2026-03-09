aa84fa73047dad176d56c0a5b448be0fcb062028
vicnot1984 <vicnot1984@gmail.com>
2026-03-09 13:16:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,9 @@ from telegram.ext import (
 # ================== НАЛАШТУВАННЯ ==================
 import os
 
-TOKEN = "8700459830:AAG2EEf4dK6tWsec5iPRIbG-SiXf2oS62yo"
+TOKEN = os.environ.get("TOKEN")
+if not TOKEN:
+    raise ValueError("TOKEN не встановлений")
 
 ADMIN_ID = 8700459830
 
